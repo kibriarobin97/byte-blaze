5dbed6eb98df4608343647362515d76d65b63169
kibriarobin97 <kibriarobin97@gmail.com>
2024-03-21 15:26:52 +0600

    added navbar section

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h3>Home</h3>
+        </div>
+    );
+};
+
+export default Home;
